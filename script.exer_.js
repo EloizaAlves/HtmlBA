5fb4edb0641907d0106ea4a9ebe915cc324afd99
EloizaAlves <eloiza.eadss.2020@gmail.com>
2023-04-11 16:01:00 -0300

    Create script.exer_.js

---
+++ script.exer_.js
@@ -0,0 +1,41 @@
+var nome='Olá mundo';
+
+let x = 10;
+let y = 5;
+let soma = x + y; 
+console.log(nome);
+console.log(x);
+console.log(y);
+console.log(soma);
+
+let w = 18
+    
+    if(w != 0){
+        console.log(w + ' O numero é impar')
+    }
+    else{
+        console.log(w + ' O numero é par')
+    }
+
+let r = 2;
+
+    if(r > 0){
+        console.log(r + ` É maior que 0 `)
+    }
+    else if (r < 0){
+        console.log(r +  `É menor que 0 `)
+    }
+    else if (r = 0){
+        console.log(r + ` É igual que 0 `)
+    }
+
+
+let t =18
+    if (t >=18){
+        console.log('Pode votar')
+    }
+    else {
+        console.log('Não pode votar')
+    }
+
+
